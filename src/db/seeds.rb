a881f00a4232bd9f6b9a9b677967c0f4481dc013
kisana <kisana@amiverse.net>
2025-06-17 18:06:25 +0900

    [HOTFIX]seed画像参照時の拡張子を反映

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -15,11 +15,11 @@ Account.create!(
     front_image: ActionDispatch::Http::UploadedFile.new(
       filename: "front_image_#{num}",
       type: "image/png",
-      tempfile: File.open(Rails.root.join("db", "seed_images", "#{num}-f.png"))),
+      tempfile: File.open(Rails.root.join("db", "seed_images", "#{num}-f.webp"))),
     back_image: ActionDispatch::Http::UploadedFile.new(
       filename: "back_image_#{num}",
       type: "image/png",
-      tempfile: File.open(Rails.root.join("db", "seed_images", "#{num}-b.png"))),
+      tempfile: File.open(Rails.root.join("db", "seed_images", "#{num}-b.webp"))),
     captured_at: Time.zone.local(2025, 6, 8, 15, 0) + num.minutes,
     status: 1,
     created_at: Time.zone.local(2025, 6, 8, 15, 0),
